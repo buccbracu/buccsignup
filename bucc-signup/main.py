64d26c22aecb3771090c1ce69d31a0d42580e727
mushfiqulIslam <nnahid878@gmail.com>
2020-02-02 21:03:28 +0600

    Added Checking validity of input

--- bucc-signup/main.py
+++ bucc-signup/main.py
@@ -1,24 +1,20 @@
 from tkinter import *
-import time
+from tkinter import messagebox
 from PIL import Image, ImageTk
 import os
-import xlsxwriter
+from openpyxl import Workbook
+import re
 
 
 
 class Signup:
     def __init__(self, master):
         #creating or loading xlx file
-        self.workbook = xlsxwriter.Workbook('buccsignup.xlsx')
-        self.worksheet = self.workbook.add_worksheet()
-        self.row = 1
-        #setting background color and text color of title
-        data_format = self.workbook.add_format({'bg_color': '#2F363F', 'font_color': 'white'})
-        self.worksheet.set_row(0, cell_format=data_format)
-        self.worksheet.write(0, 0, "Name")
-        self.worksheet.write(0, 1, "Id")
-        self.worksheet.write(0, 2, "Email")
-        self.worksheet.write(0, 3, "Number")
+        self.fname = ''
+        self.wb = Workbook()
+        self.sheet = self.wb.active
+        #setting text color of title
+        self.sheet.append(("Name", "Id", "Email", "Number"))
 
         self.master = master
         self.frame = Frame(self.master)
@@ -26,134 +22,198 @@ class Signup:
 
         self.window_width = self.master.winfo_screenwidth()
         self.window_height = self.master.winfo_screenheight()
-        self.master.geometry("750x600")
+        self.master.geometry("500x590")
+
+        #arrows and mouse button-1
+        self.master.bind('<Button-1>', self.button_1)
+        self.master.bind('<Down>', self.downKey)
+        self.master.bind('<Up>', self.upKey)
+        self.focused_entry = 0
 
         #Loading Right Background
         open = Image.open("images/background.png")
-        open = open.resize((200, (self.window_height - 200)), Image.ANTIALIAS)
+        open = open.resize((200, (self.window_height - 150)), Image.ANTIALIAS)
         image = ImageTk.PhotoImage(open)
         img = Label(self.master, image=image, borderwidth=0, bg="#EAF0F1")
         img.image = image
-        img.place(x=550, y=0)
-
-        #Loading Left Background
-        openl = Image.open("images/back-l.jpg")
-        imagel = ImageTk.PhotoImage(openl)
-        self.imgl = Label(self.master, image=imagel, borderwidth=0, bg="#EAF0F1")
-        self.imgl.image = imagel
-        self.imgl.place(x=-130, y=0)
-
-        # #Loading BUCC image
-        # open = Image.open("images/backbucc.jpg")
-        # open = open.resize((200,200), Image.ANTIALIAS)
-        # image = ImageTk.PhotoImage(open)
-        # img = Label(master, image=image, borderwidth=0)
-        # img.image = image
-        # img.place(x=50, y=15)
+        img.place(x=300, y=10)
 
         #Loading BUCC Full Name image
-        open = Image.open("images/BUCC.png")
-        # open = open.resize((200,200), Image.ANTIALIAS)
+        open = Image.open("images/ClubLogo.png")
+        open = open.resize((260, 60), Image.ANTIALIAS)
         image = ImageTk.PhotoImage(open)
         img = Label(self.master, image=image, borderwidth=0, bg="#EAF0F1")
         img.image = image
-        img.place(x=250, y=5)
+        img.place(x=120, y=20)
 
-        #Loading signup image
-        open = Image.open("images/recruit.png")
-
-        open = open.resize((150,20), Image.ANTIALIAS)
-        image = ImageTk.PhotoImage(open)
-        img = Label(self.master, image=image, borderwidth=0, bg="#EAF0F1")
-        img.image = image
-        img.place(x=420, y=50)
 
         #Loading signuplogo image
         open = Image.open("images/signup-logo.jpg")
-        open = open.resize((100,100), Image.ANTIALIAS)
+        open = open.resize((80, 80), Image.ANTIALIAS)
         image = ImageTk.PhotoImage(open)
         img = Label(self.master, image=image, borderwidth=0, bg="#EAF0F1")
         img.image = image
-        img.place(x=440, y=80)
+        img.place(x=200, y=100)
 
         #Creating Name label
         self.labelN = Label(self.master, text="Name:", font=("Times", 16), bg="#EAF0F1")
-        self.labelN.place(x=340, y=200)
+        self.labelN.place(x=90, y=200)
 
         #Taking Name Input
         self.name =StringVar()
-        self.inputN = Entry(self.master,font=("Times", 16, "italic"), justify="center",width=30, textvariable=self.name)
-        self.inputN.place(x=340, y=230)
+        self.inputN = Entry(self.master,font=("Times", 16), justify="center",width=30, textvariable=self.name)
+        self.inputN.place(x=90, y=230)
 
 
         #Creating ID label
-        self.labelI = Label(self.master, text="Student ID:", font=("serif", 16), bg="#EAF0F1")
-        self.labelI.place(x=340, y=280)
+        self.labelI = Label(self.master, text="Student ID:", font=("Times", 16), bg="#EAF0F1")
+        self.labelI.place(x=90, y=280)
 
         #Taking ID Input
         self.id =StringVar()
-        self.inputI = Entry(self.master,font=("Times", 16, "italic"), justify="center",width=30, textvariable=self.id)
-        self.inputI.place(x=340, y=310)
+        self.inputI = Entry(self.master,font=("Times", 16), justify="center",width=30, textvariable=self.id)
+        self.inputI.place(x=90, y=310)
 
         #Creating Email label
-        self.labelE = Label(self.master, text="Email:", font=("serif", 16), bg="#EAF0F1")
-        self.labelE.place(x=340, y=360)
+        self.labelE = Label(self.master, text="Email:", font=("Times", 16), bg="#EAF0F1")
+        self.labelE.place(x=90, y=360)
 
         #Taking Email Input
         self.email = StringVar()
-        self.inputE = Entry(self.master,font=("Times", 16, "italic"), justify="center",width=30, textvariable=self.email)
-        self.inputE.place(x=340, y=390)
+        self.inputE = Entry(self.master,font=("Times", 16), justify="center",width=30, textvariable=self.email)
+        self.inputE.place(x=90, y=390)
 
         #Creating Phone no label
-        self.labelP = Label(self.master, text="Mobile No:", font=("Times", 16, "italic"), bg="#EAF0F1")
-        self.labelP.place(x=340, y=440)
+        self.labelP = Label(self.master, text="Mobile No:", font=("Times", 16), bg="#EAF0F1")
+        self.labelP.place(x=90, y=440)
 
         #Taking Phone no Input
         self.number = StringVar()
-        self.inputP = Entry(self.master,font=("baskerville", 14, "italic"), justify="center",width=30, textvariable=self.number)
-        self.inputP.place(x=340, y=470)
+        self.inputP = Entry(self.master, font=("Times", 16), justify="center", width=30, textvariable=self.number)
+        self.inputP.place(x=90, y=470)
 
         #Submit Button
         btn = Button(self.master, text="Submit", font=("serif", 10, "bold"), activebackground="#3498DB", activeforeground="#758AA2", width=10, command=self.submit)
         btn.image = image
-        btn.place(x=450, y=520)
+        btn.place(x=200, y=520)
+        self.popup_success()
 
 
 
-    #checks the values and pass them for database entry quary
+    #checks the values and pass them for xlsx entry
     def submit(self):
-        if (self.name.get() == "") or (self.id.get() == "") or (self.email.get() == "") or (self.number.get() == ""):
-            openl = Image.open("images/back-wr.jpg")
-            imagel = ImageTk.PhotoImage(openl)
-            self.imgl = Label(self.master, image=imagel, borderwidth=0, bg="#EAF0F1")
-            self.imgl.image = imagel
-            self.imgl.place(x=-130, y=0)
+        name_regex = '^[A-Za-z_\s]+$'
+        id_number_regex = '^[0-9]+$'
+        email_regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+        if(re.search(name_regex, self.name.get())):
+            if(re.search(id_number_regex, self.id.get())):
+                if(re.search(email_regex, self.email.get())):
+                    if(re.search(id_number_regex, self.number.get())):
+                        #writing values into worksheet
+                        self.sheet.append((self.name.get(), self.id.get(), self.email.get(), self.number.get()))
+                        if (self.fname == ''):
+                            if(self.fname_sv.get() == ''):
+                                self.fname = "ExelFiles/buccsignup.xlsx"
+                            else:
+                                self.fname = "ExelFiles/" + self.fname_sv.get() + ".xlsx"
+                        try:
+                            self.wb.save(self.fname)
+                        except FileNotFoundError:
+                            os.mkdir('ExelFiles')
+                            self.wb.save(self.fname)
+
+                        #deleting all values from entry box
+                        self.inputN.delete('0', END)
+                        self.inputI.delete('0', END)
+                        self.inputE.delete('0', END)
+                        self.inputP.delete('0', END)
+                        messagebox.showinfo(title="Awsome", message="Thankyou\n For further informaion\n check your email")
+                    else:
+                        messagebox.showwarning(title="Invalid", message="Phone number can only contain digits (0-9)")
+                        self.inputP.delete('0', END)
+                        self.inputP.focus()
+                        self.focused_entry = 4
+                else:
+                    messagebox.showwarning(title="Invalid", message="Email consists of an email prefix and an email domain\
+                    \n  Example: bucc@gmail.com")
+                    self.inputE.delete('0', END)
+                    self.inputE.focus()
+                    self.focused_entry = 3
+            else:
+                messagebox.showwarning(title="Invalid", message="Id can only contain digits (0-9)\n  Example: 17101120")
+                self.inputI.delete('0', END)
+                self.inputI.focus()
+                self.focused_entry = 2
         else:
-            openl = Image.open("images/back-c.jpg")
-            imagel = ImageTk.PhotoImage(openl)
-            self.imgl = Label(self.master, image=imagel, borderwidth=0, bg="#EAF0F1")
-            self.imgl.image = imagel
-            self.imgl.place(x=-130, y=0)
-            #writing values into worksheet
-            self.worksheet.write(self.row, 0, self.name.get())
-            self.worksheet.write(self.row, 1, self.id.get())
-            self.worksheet.write(self.row, 2, self.email.get())
-            self.worksheet.write(self.row, 3, self.number.get())
-            self.row += 1
-            #deleting all values from entry box
+            messagebox.showwarning(title="Invalid", message="Name can only contain letters (a-z)\n  Example: Mushfiq")
             self.inputN.delete('0', END)
-            self.inputI.delete('0', END)
-            self.inputE.delete('0', END)
-            self.inputP.delete('0', END)
+            self.inputN.focus()
+            self.focused_entry = 1
+
+    #popup for exel file name
+    def popup_success(self):
+        fname = Toplevel(bg="#EAF0F1")
+        fname.geometry("300x150+240+350")
+        fname.title("Set File Name")
+        #Creating Name label
+        labelF = Label(fname, text="Write file name", font=("Times", 16), bg="#EAF0F1")
+        labelF.place(x=90, y=30)
+
+        #Taking Name Input
+        self.fname_sv = StringVar()
+        inputF = Entry(fname, font=("Times", 16), justify="center", width=20, textvariable=self.fname_sv)
+        inputF.place(x=40, y=70)
+
+        #Submit Button
+        fbtn = Button(fname, text="Submit", font=("serif", 10, "bold"), activeforeground="#758AA2", width=10, command=fname.destroy)
+        fbtn.place(x=110, y=110)
+        def y(self):
+            print(self.fname)
+
+    # controlling mouse left button
+    def button_1(self, event):
+        self.frame.focus_set()
+        if (str(self.frame.focus_get())) == ".!entry":
+            self.focused_entry = 1
+        elif (str(self.frame.focus_get())) == ".!entry2":
+            self.focused_entry = 2
+        elif (str(self.frame.focus_get())) == ".!entry3":
+            self.focused_entry = 3
+        else:
+            self.focused_entry = 4
+
+    # controlling down key
+    def downKey(self, event):
+        if (self.focused_entry) == 1:
+            self.inputI.focus()
+            self.focused_entry = 2
+        elif (self.focused_entry) == 2:
+            self.inputE.focus()
+            self.focused_entry = 3
+        elif (self.focused_entry) == 3:
+            self.inputP.focus()
+            self.focused_entry = 4
+
+    # controlling up key
+    def upKey(self, event):
+        if (self.focused_entry) == 4:
+            self.inputE.focus()
+            self.focused_entry = 3
+        elif (self.focused_entry) == 2:
+            self.inputN.focus()
+            self.focused_entry = 1
+        elif (self.focused_entry) == 3:
+            self.inputI.focus()
+            self.focused_entry = 2
 
 
 
 def main():
     root = Tk()
+    root.call('wm', 'iconphoto', root._w, ImageTk.PhotoImage(file='images/BUCC-logo.png'))
     app = Signup(root)
     root.configure(background="#EAF0F1")
     root.mainloop()
-    app.workbook.close()
 
 if __name__ == '__main__':
     main()
